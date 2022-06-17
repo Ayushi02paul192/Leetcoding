13ab4a7d5405101512d96120a8cdc32ba69a074c
Ayushi Paul <74666199+Ayushi02paul192@users.noreply.github.com>
2022-06-17 18:45:53 +0530

    Time: 204 ms (34.98%), Space: 61.3 MB (61.25%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -7,14 +7,13 @@ public:
         {
             slow=nums[slow];
             fast=nums[nums[fast]];
-        }
-        while(slow!=fast);
-        fast=nums[0];
+        }while(slow!=fast);
+        slow=nums[0];
         while(slow!=fast)
         {
             slow=nums[slow];
             fast=nums[fast];
         }
-        return slow;
+        return fast;
     }
 };
